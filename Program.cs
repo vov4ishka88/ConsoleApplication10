f9322d3c44b1aab34a2ecb0816aed3ff6e8318d1
vov4ishka88 <vov4ishka1988@gmail.com>
2015-02-05 19:18:27 +0200

    this is second commit from main branch

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Master new1 = new Master();
+            Slave new2 = new Slave();
+            new1.m1(new2);
+        }
+    }
+    class Master {
+        public void m1(Slave arg){
+            arg.m2();
+        }
+    }
+    class Slave {
+        public void m2() { 
+            Console.WriteLine("This is second method"); 
+        }
+    }
+}
